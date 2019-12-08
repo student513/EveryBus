823615952ec70fa88e0b0da5655f445fd4159965
student513 <compa513@naver.com>
2019-12-08 21:45:41 +0900

    phone complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.contrib.auth import password_validation
+from .models import Profile
 #from phonenumber_field.formfields import PhoneNumberField
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=20, label="아이디")
     email = forms.EmailField(max_length=64, label="이메일")
 
-    #phone =  PhoneNumberField()
+
     password1 = forms.CharField(
         label="비밀번호",
         strip=False,
@@ -22,4 +23,29 @@ class SignUpForm(UserCreationForm):
     )
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'email', 'password1','password2']
+        fields = ['username', 'email', 'password1','password2',]
+
+class ProfileRegsiterForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone']
+        widgets ={
+            'phone': forms.TextInput(attrs={'class':'form-control', 'placeholder':'숫자만 입력해주세요'})
+        }
+        labels ={
+            'phone': '연락처',
+        }
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+        
+class CustomUserChangeForm(UserChangeForm):
+    phone = forms.CharField(
+        label="연락처",
+        )
+    class Meta:
+        model = Profile
+        fields = ['phone',]
--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,56 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, authenticate as auth_authenticate
+
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from .forms import SignUpForm
+from .forms import SignUpForm,CustomUserChangeForm,ProfileRegsiterForm,LoginForm
+from django.http import HttpResponse
+
 def signup(request):
     if request.method == 'POST':
         #signup_form = UserCreationForm(request.POST)
         signup_form = SignUpForm(request.POST)
-        if signup_form.is_valid():
-            signup_form.save()
+        profile_form = ProfileRegsiterForm(request.POST)
+        if signup_form.is_valid() and profile_form.is_valid():
+            user = signup_form.save(commit=False)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
             return redirect('home')
     else:
         #signup_form = UserCreationForm()
         signup_form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'signup_form':signup_form})
+        profile_form = ProfileRegsiterForm()
+    return render(request, 'accounts/signup.html', {'signup_form':signup_form,'profile_form':profile_form})
+
+# def login(request):
+#     if request.method == 'POST':
+#         login_form = AuthenticationForm(request, request.POST)
+#         if login_form.is_valid():
+#             auth_login(request, login_form.get_user())
+#         return redirect('home')
+    
+#     else:
+#         login_form = AuthenticationForm()
+    
+#     return render(request, 'accounts/login.html', {'login_form' : login_form})
 
 def login(request):
     if request.method == 'POST':
-        login_form = AuthenticationForm(request, request.POST)
-        if login_form.is_valid():
-            auth_login(request, login_form.get_user())
-        return redirect('home')
-    
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth_authenticate(username = username, password = password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
     else:
-        login_form = AuthenticationForm()
-    
-    return render(request, 'accounts/login.html', {'login_form' : login_form})
-
+        form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout(request):
