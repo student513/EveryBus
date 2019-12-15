3db9758e62e30a916304ac8eaedd0bd1c7e4b31d
student513 <compa513@naver.com>
2019-12-15 16:08:14 +0900

    adsdfasdf

--- ebuapp/management/commands/createsu.py
+++ ebuapp/management/commands/createsu.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username = 'adminuser').exists():
-            User.objects.create_superuser( username = 'adminuser',
+            User.objects.create_superuser( username = 'adminuser'
             , password='passpass')
